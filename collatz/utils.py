ca3c9493b5a11b0412ee7bf89292cd1a38427e04
carlos <carlosjmnz79@gmail.com>
2020-01-08 17:58:15 -0300

    utils.py: collatz_sequence() implemented.

---
+++ collatz/utils.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def collatz( n ):
+	''' Esta función asume que recibe un número natural.'''
+
+	if not n % 2: # even
+		return n // 2 
+	else: # odd
+		return 3 * n + 1
+	
+def collatz_sequence( input ):
+	'''
+	Esta función espera un número natural,
+	si lo es retorna un string con la secuencia collatz de ese número,
+	si no lo es retorna "Input inválido.".
+	'''
+	try:
+		n = int( input )
+	except Exception as e:
+		return "Input inválido."
+
+	list_ = []
+	while not n == 1:
+		list_.append( str(n) )
+		n = collatz( n )
+	list_.append( str(n) )
+
+	return ' ,'.join( list_ ) 
