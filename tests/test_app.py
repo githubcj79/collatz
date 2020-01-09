2a83980ac938e378c08411cba08857ee0e9ea673
carlos <carlosjmnz79@gmail.com>
2020-01-09 11:11:28 -0300

    test_app.py: version 3 OK

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,10 +4,21 @@
 import requests
 from pprint import pprint 
 
+
+# try:
+# 	n = int( input )
+# except Exception as e:
+# 	abort(404)
+
+
 def test_app( url ):
-	response = requests.get( url )
-	pprint( response .status_code )
-	pprint( response.json()  )
+	try:
+		response = requests.get( url )
+		pprint( response .status_code )
+		pprint( response.json()  )
+	except Exception as e:
+		print ("Requests fallo.\n", e)
+
 
 def main():
 	test_app( "http://localhost:5000/collatz/api/v1.0/3" )
