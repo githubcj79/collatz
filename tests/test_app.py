18326a55a0aaf359d584b007249f9beace1a01bc
carlos <carlosjmnz79@gmail.com>
2020-01-09 10:59:20 -0300

    test_app.py: version 2 OK

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,14 +4,14 @@
 import requests
 from pprint import pprint 
 
-def test_app():
-	url = "http://localhost:5000/collatz/api/v1.0/3"
+def test_app( url ):
 	response = requests.get( url )
-	pprint( response )
+	pprint( response .status_code )
 	pprint( response.json()  )
 
 def main():
-	test_app()
+	test_app( "http://localhost:5000/collatz/api/v1.0/3" )
+	test_app( "http://localhost:5000/collatz/api/v1.0/bad" )
 
 if __name__ == '__main__':
 	main()
